72d8291bd330f2927c7bfad434e8102250e50ac3
Nikita Vasetskiy <tv42.vasetskiy.nikita@gmail.com>
2026-05-12 18:05:17 +0300

    Виправлено помилку пропуску початкового вікна

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../login.html'));
 });
 app.use(cors()); 
 app.use(express.json()); 
